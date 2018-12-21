c5319916f350bcf7843211f821ddfd951d3db442
Octavian Vaideanu <octavianvaideanu@Octavians-MacBook-Pro.local>
2018-12-21 08:53:48 +0200

    database config

--- lib/utils/dbConfig.ts
+++
@@ -1,9 +0,0 @@
-import {Sequelize} from 'sequelize-typescript';
-
-export const sequelize = new Sequelize({
-  username: "root",
-  password: "admin123",
-  database: "spendings",
-  dialect: "mysql",
-  modelPaths: [__dirname + '../models']
-});
